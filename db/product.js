6209e2eadf0268082a1e64742aa34d574eaa2e22
Harry Smith <harry@hsmith.net>
2021-01-16 18:17:55 +0000

    Product database add, update, delete

--- db/product.js
+++ db/product.js
@@ -17,3 +17,30 @@ export const findByCategory = async (category) => {
     .query('SELECT * FROM products WHERE products.category = $1', [category])
     .then((response) => response.rows);
 };
+
+export const add = async (product) => {
+  return await db.query('INSERT INTO products VALUES ($1, $2, $3, $4, $5)', [
+    product.id,
+    product.name,
+    product.description,
+    product.price,
+    product.category,
+  ]);
+};
+
+export const update = async (product) => {
+  return await db.query(
+    'UPDATE products SET name=$1, description=$2, price=$3, category=$4 WHERE products.id = $5',
+    [
+      product.name,
+      product.description,
+      product.price,
+      product.category,
+      product.id,
+    ]
+  );
+};
+
+export const deleteById = async (id) => {
+  return await db.query('DELETE FROM products WHERE products.id = $1', [id]);
+};
--- tests/product-db.test.js
+++ tests/product-db.test.js
@@ -6,7 +6,14 @@ import { seedData, removeSeedData } from './helpers/product.js';
 import db from '../db/db.js';
 import createServer from '../app.js';
 
-import { getAll, findById, findByCategory } from '../db/product.js';
+import {
+  getAll,
+  findById,
+  findByCategory,
+  add,
+  update,
+  deleteById,
+} from '../db/product.js';
 
 const app = createServer();
 const api = await request(app);
@@ -58,5 +65,41 @@ describe('Product Database Logic', () => {
     const products = await findByCategory(2);
 
     products.should.deep.equal([productData[1], productData[2]]);
+  });
+  it('adds a new product to the database', async () => {
+    const newProduct = {
+      id: nanoid(),
+      name: 'test3',
+      description: 'adding a product',
+      price: 2000,
+      category: 3,
+    };
+
+    await add(newProduct);
+
+    const products = await getAll();
+    products.length.should.equal(4);
+  });
+  it('updates a product in the database', async () => {
+    const updatedProduct = {
+      id: productData[0].id,
+      name: 'test-updated',
+      description: 'description-updated',
+      price: '$2,500.00',
+      category: 1,
+    };
+
+    await update(updatedProduct);
+
+    const products = await getAll();
+    const product = await findById(updatedProduct.id);
+    products.length.should.equal(3);
+    product.should.deep.equal(updatedProduct);
+  });
+  it('delete a product in the database by id', async () => {
+    await deleteById(productData[0].id);
+
+    const products = await getAll();
+    products.length.should.equal(2);
   });
 });
